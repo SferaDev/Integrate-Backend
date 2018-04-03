d7b739826988dcd14f0b178e3b63b3996f51c188
Alexis Rico <SferaDev@users.noreply.github.com>
2018-04-03 09:05:15 +0200

    Routes: Remove comment

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,7 +3,6 @@
 module.exports = function(app) {
     var beneficiary = require('../controllers/BeneficiaryController');
 
-    // todoList Routes
     app.route('/beneficiaries')
         .post(beneficiary.loadBeneficiaries)
 };
