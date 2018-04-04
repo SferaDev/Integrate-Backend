28faebda5991afae337e8891e1a6c1f33034fddd
Alexis Rico <sferadev@gmail.com>
2018-04-04 08:27:34 +0200

    Remove vars and let the const begin

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function(app) {
-    var beneficiary = require('../controllers/BeneficiaryController');
+    const beneficiaryController = require('../controllers/BeneficiaryController');
 
     app.route('/beneficiaries')
-        .post(beneficiary.loadBeneficiaries)
+        .post(beneficiaryController.loadBeneficiaries)
 };
