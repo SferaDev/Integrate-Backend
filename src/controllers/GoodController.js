70a4a150a7301ebee7a9e82582d3b2d7239cf3c0
Aleix Balletbo <aleix.balletbo@hotmail.com>
2018-04-12 22:51:14 +0200

    feature/6: Add creation of goods (#15)

---
+++ src/controllers/GoodController.js
@@ -0,0 +1,17 @@
+import {entityModel} from "../models/UserModel";
+import {goodModel} from "../models/GoodModel";
+
+exports.addGood = function (req, res) {
+    if (req.userType === 'Entity') {
+        entityModel.findOne({email: req.userId}, function (err, entity) {
+            if (err == null)
+                req.body.userId = entity._id;
+            let newGood = new goodModel(req.body);
+            newGood.save(function (err, good) {
+                if (err)
+                    res.status(500).send(err);
+                res.status(201).send(good);
+            });
+        });
+    }
+};
