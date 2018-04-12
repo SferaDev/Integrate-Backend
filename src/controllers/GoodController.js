617f0644130059c26ee0c35b6dddf802992d42e8
Aleix Balletbó <aleix.balletbo@est.fib.upc.edu>
2018-04-12 20:30:17 +0200

    Fix userId of goods

--- src/controllers/GoodController.js
+++ src/controllers/GoodController.js
@@ -1,13 +1,20 @@
+import {entityModel} from "../models/UserModel";
+
 const mongoose = require('mongoose');
 const Good = mongoose.model('Good');
 
 exports.addGood = function (req, res) {
     if (req.userType === 'Entity') {
-        let newGood = new Good(req.body);
-        newGood.save(function (err, good) {
-            if (err)
-                res.status(500).send(err);
-            res.status(201).send(good);
+        entityModel.findOne({email: req.userId}, function (err, entity) {
+            if (err == null)
+                req.body.userId = entity._id;
+            let newGood = new Good(req.body);
+            console.log(newGood);
+            newGood.save(function (err, good) {
+                if (err)
+                    res.status(500).send(err);
+                res.status(201).send(good);
+            });
         });
     }
 };
