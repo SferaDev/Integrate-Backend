813c7575692b819715b5bfe837d7154e8d79da69
Aleix Balletbo <aleix.balletbo@hotmail.com>
2018-04-13 11:07:13 +0200

    fix/getEntities: Add projection (#20)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -52,7 +52,7 @@ exports.loginUser = function (req, res) {
 };
 
 exports.getEntities = function (req, res) {
-    entityModel.find(function (err, entities) {
+    entityModel.find({}, {name: 1, description: 1, addressName: 1, addressLatitude: 1, addressLongitude: 1, phone: 1, picture: 1}, function (err, entities) {
         if (err) res.status(500).send(err);
         else res.status(200).send(entities);
     });
