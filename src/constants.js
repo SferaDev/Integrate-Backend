f8f49ab886abab7cb3b06d4497fc77e652d00b65
Alexis Rico <SferaDev@users.noreply.github.com>
2018-04-20 09:22:45 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,3 @@ export const ENV = process.env.NODE_ENV || 'test';
 
 // MongoDB
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Integrate';
-
-// Nodemailer
-export const EMAIL_SERVICE = 'gmail';
-export const EMAIL_USER = 'integrate.upc@gmail.com';
-export const EMAIL_PASS = '';
