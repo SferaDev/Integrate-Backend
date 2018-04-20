81b7e1dda39af1b8bc53e10613f91e0726e4c158
Alexis Rico <SferaDev@users.noreply.github.com>
2018-04-20 09:25:21 +0200

    fix/imports: Refactor imports and constants (#27)

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// Test environment
+export const PORT = process.env.PORT || 3000;
+export const ENV = process.env.NODE_ENV || 'test';
+
+// MongoDB
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Integrate';
