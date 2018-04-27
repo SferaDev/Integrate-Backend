6f27f5e547f2238ab386441925b28c14ba415e9f
Alexis Rico <SferaDev@users.noreply.github.com>
2018-04-27 10:47:32 +0200

    fix/tokencors: Modify CORS logic (#32)

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import {PORT} from "./src/constants";
 import database from "./common/database";
 
@@ -14,11 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Enable CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 // Load routes
 app.use('/', express.static('apidoc'));
