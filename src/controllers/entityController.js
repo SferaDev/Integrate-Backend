d7f221347e6d3223faab0e549745d0728c2db0c4
Aleix Balletbo <aleix.balletbo@hotmail.com>
2018-05-02 01:33:59 +0200

    feature/20: Sorting and filters for entities and goods (#39)
    * Add filters and sorting to goods
    
    * Fix entity coordinates field
    
    * Fix entity coordinates field (again)
    
    * Add sorting by proximity to etities
    
    * Update goodController.js
    
    * Update goodModel.js

--- src/controllers/entityController.js
+++ src/controllers/entityController.js
@@ -1,20 +1,48 @@
 import {entityModel} from "../models/entityModel";
-import {STATUS_FORBIDDEN, STATUS_OK, STATUS_SERVER_ERROR} from "../constants";
+import {STATUS_BAD_REQUEST, STATUS_FORBIDDEN, STATUS_OK, STATUS_SERVER_ERROR} from "../constants";
 
 exports.getEntities = function (req, res) {
     if (req.userType === 'Beneficiary') {
-        entityModel.find({}, {
+        let latitude = req.query.latitude;
+        let longitude = req.query.longitude;
+        if (!latitude || !longitude)
+            res.status(STATUS_BAD_REQUEST).send({message: "Missing query parameters"});
+        else {
+            // Build the query
+            let aggregate = entityModel.aggregate();
+            aggregate.near({
+                near: {type: "Point", coordinates: [parseFloat(longitude), parseFloat(latitude)] },
+                distanceField: "distance",
+                spherical: true
+            });
+            aggregate.project({
+                name: 1,
+                description: 1,
+                addressName: 1,
+                coordinates: 1,
+                phone: 1,
+                picture: 1,
+                distance: 1
+            });
+            // Execute the query
+            aggregate.exec(function (err, entities) {
+                if (err) res.status(STATUS_SERVER_ERROR).send(err);
+                else res.status(STATUS_OK).send(entities);
+            });
+        }
+        /*
+        entityModel.find({coordinates: {'$near': [parseFloat(longitude), parseFloat(latitude)]}}, {
             name: 1,
             description: 1,
             addressName: 1,
-            addressLatitude: 1,
-            addressLongitude: 1,
+            coordinates: 1,
             phone: 1,
             picture: 1
         }, function (err, entities) {
             if (err) res.status(STATUS_SERVER_ERROR).send(err);
             else res.status(STATUS_OK).send(entities);
         });
+        */
     } else {
         res.status(STATUS_FORBIDDEN).send({message: "You are not allowed to do this action"});
     }
