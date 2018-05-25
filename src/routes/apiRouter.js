73c68fb56ed85b3aa2d258de237e6886756fe287
Alexis Rico <sferadev@gmail.com>
2018-05-25 10:24:47 +0200

    Reorder middlewares

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -13,27 +13,6 @@ import {userModel} from "../models/userModel";
 
 export const apiRouter = express.Router();
 
-// Middleware to prettify/filter/sort JSON result
-apiRouter.use(function (req, res, next) {
-    let oldSend = res.send;
-    res.set('Content-Type', 'application/json');
-    res.send = function (obj) {
-        if (typeof obj === 'object') {
-            let attributes = req.query.filter ? req.query.filter.split(',') : null;
-            let indent = !isNaN(parseInt(req.query.indent)) ? parseInt(req.query.indent) : 0;
-            if (Array.isArray(obj) && req.query.sort) {
-                obj.sort(function (a, b) {
-                    if (a[req.query.sort] > b[req.query.sort]) return 1;
-                    else if (a[req.query.sort] < b[req.query.sort]) return -1;
-                    return 0;
-                });
-            }
-            oldSend.call(this, JSON.stringify(obj, attributes, indent));
-        } else oldSend.call(this, obj);
-    };
-    next();
-});
-
 // Middleware to verify token
 apiRouter.use(function (req, res, next) {
     // check header or url parameters or post parameters for token
@@ -63,6 +42,27 @@ apiRouter.use(function (req, res, next) {
             message: 'No token provided.'
         });
     }
+});
+
+// Middleware to prettify/filter/sort JSON result
+apiRouter.use(function (req, res, next) {
+    let oldSend = res.send;
+    res.set('Content-Type', 'application/json');
+    res.send = function (obj) {
+        if (typeof obj === 'object') {
+            let attributes = req.query.filter ? req.query.filter.split(',') : null;
+            let indent = !isNaN(parseInt(req.query.indent)) ? parseInt(req.query.indent) : 0;
+            if (Array.isArray(obj) && req.query.sort) {
+                obj.sort(function (a, b) {
+                    if (a[req.query.sort] > b[req.query.sort]) return 1;
+                    else if (a[req.query.sort] < b[req.query.sort]) return -1;
+                    return 0;
+                });
+            }
+            oldSend.call(this, JSON.stringify(obj, attributes, indent));
+        } else oldSend.call(this, obj);
+    };
+    next();
 });
 
 // Middleware to auto-translate object results
