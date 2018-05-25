e5a593a90c41797124e25361af16e74b539d6019
Alexis Rico <sferadev@gmail.com>
2018-05-25 10:59:54 +0200

    Sentence case logic

--- src/controllers/languageController.js
+++ src/controllers/languageController.js
@@ -62,8 +62,18 @@ export function translateString(language, string, callback) {
     return axios.get("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=" +
         language + "&dt=t&q=" + encodeURI(string))
     .then(function (response) {
-        callback(null, response.data[0][0][0]);
+        callback(null, sentenceCase(response.data[0][0][0]));
     }).catch(function (error) {
         callback(error, null);
     });
+}
+
+function sentenceCase(string) {
+    // Remove duplicated spacings
+    string = string.replace(/\s+/g,' ').replace(/^\s+|\s+$/,'');
+    // Upper case first letter
+    string = string.charAt(0).toUpperCase() + string.slice(1);
+    // Upper case after periods, question marks...
+    string = string.replace(/([!?.]\s+)([a-z])/g, (m, $1, $2) => $1 + $2.toUpperCase());
+    return string;
 }
